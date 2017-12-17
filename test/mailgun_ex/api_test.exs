192ad56148c39b70a6287012592c529df6fa22c2
Andrew Forward <aforward@gmail.com>
2017-12-17 16:53:33 -0500

    Write domains output to a file when touching the legit service

--- test/mailgun_ex/api_test.exs
+++ test/mailgun_ex/api_test.exs
@@ -15,16 +15,18 @@ defmodule MailgunEx.ApiTest do
 
   @tag :external
   test "Make a live request to Mailgun API" do
-    {ok, _data} = Api.request(
+    {ok, data} = Api.request(
                   :get,
                   resource: "domains",
                   api_key: "key-3ax6xnjp29jd6fds4gc373sgvjxteol0")
     assert 200 == ok
+    File.mkdir_p("./test/fixtures")
+    File.write("./test/fixtures/domains.json", data)
   end
 
   test "/domains", %{bypass: bypass} do
     Bypass.expect_once bypass, "GET", "/domains", fn conn ->
-      Plug.Conn.resp(conn, 200, ~s<xxx>)
+      Plug.Conn.resp(conn, 200, File.read!("./test/fixtures/domains.json"))
     end
     {ok, _data} = Api.request(:get,
                     base: "http://localhost:#{bypass.port}",
